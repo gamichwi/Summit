722a70733745e87d17f20e6b89cad45fec0a74f6
Gavin Williams <gamichwi@gmail.com>
2020-01-27 21:59:21 +1100

    added place specific and user specific routes. Included error handling.

--- summit-back-end/app.js
+++ summit-back-end/app.js
@@ -5,6 +5,14 @@ const summitsRoutes = require('./routes/summits-routes')
 
 const app = express();
 
-app.use(summitsRoutes);
+app.use('/api/summits', summitsRoutes);
+
+app.use((error, req, res, next) => {
+    if(res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message: error.message || 'An unknown error has occured.'})
+});
 
 app.listen(5000);
--- summit-back-end/routes/summits-routes.js
+++ summit-back-end/routes/summits-routes.js
@@ -1,10 +1,79 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res, next)=> {
-    console.log('GET request in places');
-    res.json({message: 'It works!'})
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    userId: "u1",
+    title: "Win a foosball championship",
+    completedAddress: "Bartronica, Melbourne",
+    setDate: "",
+    targetDate: "",
+    completedDate: "",
+    completed: false,
+    setImageUrl: "https://i.ytimg.com/vi/xXq0KPEJuBk/maxresdefault.jpg",
+    completedImageUrl:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.z1EQMkYXiRUgWV9rkGpT9AHaE8%26pid%3DApi&f=1",
+    setCoordinates: {},
+    targetCoordinates: {},
+    completedCoordinates: {
+      lat: "-37.8785909",
+      lng: "144.9739747"
+    },
+    likes: [],
+    summitWord: [],
+    private: true
+  },
+  {
+    id: "p2",
+    userId: "u2",
+    title: "Win a boxing championship",
+    completedAddress: "Melbourne Fight Club",
+    setDate: "",
+    targetDate: "",
+    completedDate: "",
+    completed: false,
+    setImageUrl:
+      "https://media2.trover.com/T/58c263cd74eb4650f0022ede/fixedw_large_4x.jpg",
+    completedImageUrl:
+      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201910/jamuna9102019-770x433.png?VB4oJ8dItSqFhwwpLlkhrdCDSzixJd4w",
+    setCoordinates: {},
+    targetCoordinates: {},
+    completedCoordinates: {
+      lat: "-37.7873385",
+      lng: "144.8642536"
+    },
+    likes: [],
+    summitWord: [],
+    private: true
+  }
+];
+
+router.get("/:summitId", (req, res, next) => {
+  const summitId = req.params.summitId;
+  const summit = DUMMY_PLACES.find(s => {
+    return s.id === summitId;
+  });
+  if (!summit) {
+      const error = new Error('Could not find a place for the provided summit id.');
+      error.code = 404;
+      return next(error);
+  }
+  res.json({ summit });
+});
+
+router.get("/user/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+  const summit = DUMMY_PLACES.find(s => {
+    return s.userId === userId;
+  });
+  if (!summit) {
+    const error = new Error('Could not find a place for the provided user id.');
+    error.code = 404;
+    return next(error);
+}
+  res.json({ summit });
 });
 
 module.exports = router;
