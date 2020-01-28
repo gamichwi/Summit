c25766f2864c7cdd3a9a15b33a40763de54c65b1
Gavin Williams <gamichwi@gmail.com>
2020-01-28 22:26:52 +1100

    added user routes

---
+++ summit-back-end/controllers/users-controllers.js
@@ -0,0 +1,57 @@
+const uuid = require("uuid/v4");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "Gavin Williams",
+    email: "gavin@gavin.com",
+    password: "pass"
+  },
+  {
+    id: "u2",
+    name: "Williams Gavin",
+    email: "test@test.com",
+    password: "test"
+  }
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find(u => u.email === email);
+  if(hasUser) {
+    return next(
+      new HttpError("Can not create a new user. That email already exists.", 422));
+
+  }
+
+  const createdUser = {
+    id: uuid(),
+    name,
+    email,
+    password
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+  if (!identifiedUser || identifiedUser.password != password) {
+    return next(
+      new HttpError("Login error. Please check your login details.", 401));
+  }
+  res.json('Logged in!')
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
