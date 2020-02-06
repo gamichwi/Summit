3a11017ae7847bf742a5c356f1ed8f60365544c7
Gavin Williams <gamichwi@gmail.com>
2020-02-06 20:17:49 +1100

    created connection to mongoDB

--- summit-back-end/app.js
+++ summit-back-end/app.js
@@ -1,29 +1,38 @@
-require('dotenv').config()
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const summitsRoutes = require('./routes/summits-routes')
-const usersRoutes = require('./routes/users-routes')
-const HttpError = require('./models/http-error');
+const summitsRoutes = require("./routes/summits-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/summits', summitsRoutes);
-app.use('/api/users', usersRoutes);
+app.use("/api/summits", summitsRoutes);
+app.use("/api/users", usersRoutes);
 
-app.use((req, res,  next) => {
-    const error = new HttpError('Could not find this route.', 404);
-    return next(error);
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  return next(error);
 });
 
 app.use((error, req, res, next) => {
-    if(res.headerSent) {
-        return next(error);
-    }
-    res.status(error.code || 500);
-    res.json({message: error.message || 'An unknown error has occured.'})
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error has occured." });
 });
 
-app.listen(5000);
+mongoose
+  .connect('mongodb+srv://Gavin:<mongodb+srv://Gavin:Summit123@cluster0-z3zxc.mongodb.net/summits?retryWrites=true&w=majority>@cluster0-z3zxc.mongodb.net/test?retryWrites=true&w=majority')
+  .then(()=> {
+      app.listen(5000)
+    })
+  .catch(error => {
+      console.log(error)
+  });
+
