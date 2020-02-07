f3e58de81d42560069ffe403a09f3095221733b8
Gavin Williams <gamichwi@gmail.com>
2020-02-08 10:42:41 +1100

    User signup connection to the database works.

--- summit-back-end/controllers/users-controllers.js
+++ summit-back-end/controllers/users-controllers.js
@@ -2,6 +2,7 @@ const uuid = require("uuid/v4");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const DUMMY_USERS = [
   {
@@ -22,12 +23,11 @@ const getUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   // check any validation errors set with express-validator
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log(errors);
     return next(
       new HttpError(
         "Invalid data entered. Please check what you have entered.",
@@ -35,29 +35,41 @@ const signup = (req, res, next) => {
       )
     );
   }
+  const { name, email, password, summits, friends } = req.body;
 
-  const { name, email, password } = req.body;
-
-  const hasUser = DUMMY_USERS.find(u => u.email === email);
-  if (hasUser) {
-    return next(
-      new HttpError(
-        "Can not create a new user. That email already exists.",
-        422
-      )
-    );
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Signup failed. Please try again later.", 500);
+    return next(error);
   }
-  const createdUser = {
-    id: uuid(),
+
+  if (existingUser) {
+    const error = new HttpError("That user already exists. Please login.", 422);
+    return next(error);
+  }
+
+  const createdUser = new User({
     name,
     email,
-    password
-  };
+    password,
+    image:
+      "https://pngriver.com/wp-content/uploads/2018/04/Download-Male-User-Logo-Png-Image-69558-For-Designing-Projects.png",
+    summits,
+    friends
+  });
 
-  DUMMY_USERS.push(createdUser);
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError("Sign up failed, please try again.", 500);
+    return next(error);
+  }
 
-  res.status(201).json({ user: createdUser });
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) }); //converts to default javascript object and removes the _
 };
+
 const login = (req, res, next) => {
   const { email, password } = req.body;
 
