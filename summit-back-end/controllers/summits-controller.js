5f977227fd0cb37e44fb2bb7fe6e52648da1e5b1
Gavin Williams <gamichwi@gmail.com>
2020-02-07 21:41:24 +1100

    update summit connected to db

--- summit-back-end/controllers/summits-controller.js
+++ summit-back-end/controllers/summits-controller.js
@@ -155,11 +155,10 @@ const createSummit = async (req, res, next) => {
   res.status(201).json({ summit: createdSummit });
 };
 
-const updateSummit = (req, res, next) => {
+const updateSummit = async (req, res, next) => {
   // check any validation errors set with express-validator
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors);
     return next(
       new HttpError(
         "Invalid data entered. Please check what you have entered.",
@@ -171,16 +170,33 @@ const updateSummit = (req, res, next) => {
   const { title, targetAddress, targetCoordinates, targetDate } = req.body;
   const summitId = req.params.summitId;
 
-  const updatedSummit = { ...DUMMY_PLACES.find(s => s.id === summitId) };
-  const summitIndex = DUMMY_PLACES.findIndex(s => s.id === summitId);
-  updatedSummit.title = title;
-  updatedSummit.targetAddress = targetAddress;
-  updatedSummit.targetCoordinates = targetCoordinates;
-  updatedSummit.targetDate = targetDate;
+let summit;
+try{
+  summit = await Summit.findById(summitId);
+} catch(err) {
+  const error = new HttpError(
+    "An error has occured. Please try again later.",
+    500
+  );
+  return next(error);
+}
 
-  DUMMY_PLACES[summitIndex] = updatedSummit;
+  summit.title = title;
+  summit.targetAddress = targetAddress;
+  summit.targetCoordinates = targetCoordinates;
+  summit.targetDate = targetDate;
 
-  res.status(200).json({ summit: updatedSummit });
+  try {
+    await summit.save();
+  } catch(err){
+    const error = new HttpError(
+      "An error has occured. Please try again later.",
+      500
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ summit: summit.toObject({ getters: true }) });
 };
 
 const deleteSummit = (req, res, next) => {
