85b4276f88b45c242308e6fabd8ad60a26467f51
Gavin Williams <gamichwi@gmail.com>
2020-02-07 19:58:54 +1100

    Added set date to be date.now()

--- summit-back-end/controllers/summits-controller.js
+++ summit-back-end/controllers/summits-controller.js
@@ -116,7 +116,7 @@ const createSummit = async (req, res, next) => {
 
   const createdSummit = new Summit({
     title,
-    setDate: "01/01/20",
+    setDate: Date.now(),
     setImage:
       "https://images.unsplash.com/photo-1579191203631-368691293d7a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
     targetAddress,
