24f3a566d70393a288ed8f775e2e44cd0e03bc51
Gavin Williams <gamichwi@gmail.com>
2020-03-29 13:10:58 +1100

    deploying on zeit due to folder structure.

--- summit-back-end/app.js
+++ summit-back-end/app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const fs = require('fs'); //interact with files.. delete etc.
-const path = require('path'); //absolute path
+const fs = require("fs"); //interact with files.. delete etc.
+const path = require("path"); //absolute path
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -10,12 +10,12 @@ const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
-app.use(express.static('frontend'));
+app.use(express.static("frontend"));
 
 app.use(bodyParser.json());
 
 //for all urls related to images middleware
-app.use('/uploads/images', express.static(path.join('uploads','images')));
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 // headers to be added to the responses to avoid CORS error
 app.use((req, res, next) => {
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   ); //Which headers to be handled on incoming requests
-  res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); //allow http requests
+  res.setHeader("Acces-Control-Allow-Methods", "GET, POST, PATCH, DELETE"); //allow http requests
 
   next();
 });
@@ -38,10 +38,11 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-  if (req.file) { //added file property from multer
-    fs.unlink(req.file.path, (err) => {
+  if (req.file) {
+    //added file property from multer
+    fs.unlink(req.file.path, err => {
       console.log(err);
-    });//deletes file
+    }); //deletes file
   }
   if (res.headerSent) {
     return next(error);
@@ -50,9 +51,10 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error has occured." });
 });
 
+const mongoConnection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-z3zxc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-z3zxc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    mongoConnection,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
---
+++ summit-back-end/public/precache-manifest.7c967efce4fa586637afa54bdf92f86b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb639a6e5c91170452c6887115a7e0a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c5f2bddbf1167c15b1a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0089da26ff3529ae6c75",
+    "url": "/static/css/3.a3ee9efe.chunk.css"
+  },
+  {
+    "revision": "c28b200b326f3177b8c5",
+    "url": "/static/css/4.a3ee9efe.chunk.css"
+  },
+  {
+    "revision": "dd0e6e17af770aa55308",
+    "url": "/static/css/main.124d35a5.chunk.css"
+  },
+  {
+    "revision": "6c5f2bddbf1167c15b1a",
+    "url": "/static/js/2.73019aa9.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.73019aa9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0089da26ff3529ae6c75",
+    "url": "/static/js/3.d06eff28.chunk.js"
+  },
+  {
+    "revision": "c28b200b326f3177b8c5",
+    "url": "/static/js/4.3c90518d.chunk.js"
+  },
+  {
+    "revision": "74a8d77da8398e0ce072",
+    "url": "/static/js/5.92e19099.chunk.js"
+  },
+  {
+    "revision": "68b5f14e3192f46dee3e",
+    "url": "/static/js/6.bcafb5ca.chunk.js"
+  },
+  {
+    "revision": "2151421ddd16f8af52fc",
+    "url": "/static/js/7.0c56de05.chunk.js"
+  },
+  {
+    "revision": "dd0e6e17af770aa55308",
+    "url": "/static/js/main.22e8a2dc.chunk.js"
+  },
+  {
+    "revision": "a22ef702cbb8790ca2ca",
+    "url": "/static/js/runtime-main.e5af6abf.js"
+  }
+]);
